ba96b07287192dc458e71113ad5b7f236e8b5c6c
BoogiemanNSK <calah47@yandex.ru>
2020-12-14 22:15:10 +0700

    Added more calls and some other stuff

--- app/src/main/java/com/example/finalproject/data/ChallengeRepository.kt
+++ app/src/main/java/com/example/finalproject/data/ChallengeRepository.kt
@@ -1,5 +1,6 @@
 package com.example.finalproject.data
 
+import com.example.finalproject.data.requests.NewChallengeRequest
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
@@ -16,6 +17,39 @@ class ChallengeRepository(
         }, { error ->
             error.printStackTrace()
         })
+    }
+
+    fun getMyTakenChallenges() {
+        challengeApi.getMyChallenges()
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
+        .subscribe ({
+           // TODO Do something with result
+        }, { error ->
+            error.printStackTrace()
+        })
+    }
+
+    fun getMyCreatedChallenges() {
+        challengeApi.getChallenges()
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
+        .subscribe ({
+           // TODO Do something with result
+        }, { error ->
+            error.printStackTrace()
+        })
+    }
+
+    fun postNewChallenge(name: String, description: String, requirements: String, tags: Array<String>) {
+        challengeApi.createNewChallenge(
+            NewChallengeRequest(
+                name,
+                description,
+                requirements,
+                tags
+            )
+        ).execute()
     }
 
 }
