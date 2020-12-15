1c1eb2662f8f80ac520baebf8b4fbb70875610e5
BoogiemanNSK <calah47@yandex.ru>
2020-12-15 16:04:32 +0700

    Fixed search request

--- app/src/main/java/com/example/finalproject/view/SearchFragment.kt
+++ app/src/main/java/com/example/finalproject/view/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -14,6 +15,8 @@ import com.example.finalproject.data.ChallengeRepository
 import com.example.finalproject.data.interceptors.TokenInterceptor
 import com.example.finalproject.databinding.FragmentSearchBinding
 import com.example.finalproject.view.adapters.ChallengeItemAdapter
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_search.*
 import kotlinx.android.synthetic.main.fragment_search.view.*
 import okhttp3.OkHttpClient
@@ -47,50 +50,37 @@ class SearchFragment : Fragment() {
         val challengeItemAdapter = ChallengeItemAdapter()
         binding.recyclerViewSearch.adapter = challengeItemAdapter
 
-       // viewModel.allChallenges.observe(this.viewLifecycleOwner, Observer { challenges ->
+        // viewModel.allChallenges.observe(this.viewLifecycleOwner, Observer { challenges ->
         //    challengeItemAdapter.setChallenges(challenges)
-       // } )
+        // } )
 
-        val client = OkHttpClient.Builder()
-            .connectTimeout(60, TimeUnit.SECONDS)
-            .readTimeout(60, TimeUnit.SECONDS)
-            .build()
         val retrofit = Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
         val api = retrofit.create(ChallengeApi::class.java)
-        challengeRepository = ChallengeRepository(api)
 
-        binding.root.search_edit_text.doAfterTextChanged {
-            val challenges = challengeRepository.getAllChallenges(
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiMTIzNDUxMiIsInVpZCI6MiwiZXhwIjoxNjA4Mjc1NDk5fQ.5kOkYd4blAXSuZaoLCN_9oUbWBtQ0XVBLFW-vh1oSS0"
-            ).value!!
-            challengeItemAdapter.setChallenges(challenges)
-        }
+        val disposable = api.getChallenges("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiMTIzNDUxMiIsInVpZCI6MiwiZXhwIjoxNjA4Mjc1NDk5fQ.5kOkYd4blAXSuZaoLCN_9oUbWBtQ0XVBLFW-vh1oSS0")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {result ->
+                    challengeItemAdapter.setChallenges(result.challengesArray.toList())
+                },
+                { error ->
+                    Toast.makeText(context, "Request failed", Toast.LENGTH_SHORT).show()
+                }
+            )
+
+        //binding.root.search_edit_text.doAfterTextChanged {
+        //    val challenges = challengeRepository.getAllChallenges(
+        //        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiMTIzNDUxMiIsInVpZCI6MiwiZXhwIjoxNjA4Mjc1NDk5fQ.5kOkYd4blAXSuZaoLCN_9oUbWBtQ0XVBLFW-vh1oSS0"
+        //    ).value!!
+        //    challengeItemAdapter.setChallenges(challenges)
+        //}
 
         return binding.root
     }
-
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        // Add OnTextChange listener to textBox
-//        search_edit_text.addTextChangedListener(object : TextWatcher {
-//            override fun afterTextChanged(s: Editable?) {}
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                // TODO Update list of challenges from backend
-//                // TODO Add onClickListener on each challenge -> toViewChallengeActivity
-//            }
-//        })
-//    }
-//
-//    private fun toViewChallengeActivity(challengeId: Int) {
-//        val activity = (requireActivity() as MainActivity)
-//        activity.viewChallengeInfo(challengeId)
-//    }
 
 }
